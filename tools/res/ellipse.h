6d659df5cedf9fca85c52b5765048fe6ed48fa16
Tobias Weber <tobias.weber@frm2.tum.de>
2013-05-14 11:08:56 +0200

    plotted ellipses

--- tools/res/ellipse.h
+++ tools/res/ellipse.h
@@ -13,18 +13,31 @@
 
 #include <string>
 #include <ostream>
+#include <cmath>
+
+#include "../../fitter/fitter.h"
 
 #include <boost/numeric/ublas/vector.hpp>
 #include <boost/numeric/ublas/matrix.hpp>
 namespace ublas = boost::numeric::ublas;
 
-struct Ellipse
+struct Ellipse : public FunctionModel_param
 {
         double phi;
         double x_hwhm, y_hwhm;
         double x_offs, y_offs;
 
         std::string x_lab, y_lab;
+
+        virtual ublas::vector<double> operator()(double t) const
+        {
+        	ublas::vector<double> vec(2);
+
+            vec[0] = x_hwhm*std::cos(2.*M_PI*t)*std::cos(phi) - y_hwhm*std::sin(2.*M_PI*t)*std::sin(phi) + x_offs;
+            vec[1] = x_hwhm*std::cos(2.*M_PI*t)*std::sin(phi) + y_hwhm*std::sin(2.*M_PI*t)*std::cos(phi) + y_offs;
+
+            return vec;
+        }
 };
 
 extern std::ostream& operator<<(std::ostream& ostr, const Ellipse& ell);
