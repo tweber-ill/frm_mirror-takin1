aa1717fd89bf4e89674381003b52c9055f7a2f69
Tobias Weber <tobias.weber@frm2.tum.de>
2015-09-02 17:04:41 +0200

    added gcc version check

--- build.sh
+++ build.sh
@@ -1,6 +1,25 @@
 #!/bin/bash
 
 
+GCC=$(which gcc 2>/dev/null)
+
+if [ "$GCC" != "" ]	# don't check version if only e.g. clang is available
+then
+	GCC_VER=$($GCC -dumpversion)
+	echo -e "GCC is version $GCC_VER"
+
+	GCC_VER=(${GCC_VER//./ })
+
+	if [ ${GCC_VER[0]} -le 4 ] && [ ${GCC_VER[1]} -lt 8 ]
+	then
+    		echo -e "\t... which is not compatible with Takin.\n\t\tAt least version 4.8 is needed."
+	        exit -1;
+	fi
+fi
+
+
+
+
 if [ ! -d tlibs ] || [ ! -f tlibs/AUTHORS ]
 then
 	echo -e "Error: tlibs not installed. Use ./setup_tlibs.sh"
