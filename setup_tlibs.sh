51da85b8fcb6d9e4d36d330c7f001d0e3a4e21dc
Tobias Weber <tobias.weber@frm2.tum.de>
2015-10-09 13:33:33 +0200

    cosmetic change to setup script

--- setup_tlibs.sh
+++ setup_tlibs.sh
@@ -8,27 +8,29 @@ fi
 
 
 REPO=http://forge.frm2.tum.de/cgit/cgit.cgi/frm2/mira/tlibs.git/snapshot
+
 TLIBS_DIR=tlibs
+THEARCH=.tar.bz2
 
 function dl_tlibs
 {
 	echo -e "Downloading tlibs..."
-	rm -f ${TLIBS}
+	rm -f ${TLIBS}${THEARCH}
 
-	if ! wget ${REPO}/${TLIBS}.tar.bz2
+	if ! wget ${REPO}/${TLIBS}${THEARCH}
 	then
 		echo -e "Error: Cannot download tlibs.";
 		exit -1;
 	fi
 
-	if ! tar -xjvf ${TLIBS}.tar.bz2
+	if ! tar -xjvf ${TLIBS}${THEARCH}
 	then
 		echo -e "Error: Cannot extract tlibs.";
 		exit -1;
 	fi
 
 	mv ${TLIBS} ${TLIBS_DIR}
-	rm -f ${TLIBS}.tar.bz2
+	rm -f ${TLIBS}${THEARCH}
 }
 
 
