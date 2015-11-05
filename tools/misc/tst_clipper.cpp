6679c4c3920e8ba5324f820573aa6158c1463037
Tobias Weber <tobias.weber@frm2.tum.de>
2015-11-05 11:37:30 +0100

    tested form factors

--- tools/misc/tst_clipper.cpp
+++ tools/misc/tst_clipper.cpp
@@ -1,5 +1,9 @@
-// gcc -o tst_clipper tst_clipper.cpp -lstdc++ -lclipper-core
+// gcc -o tst_clipper tst_clipper.cpp -lstdc++ -lm -lclipper-core -std=c++11
+
 #include <clipper/clipper.h>
+#include "../../tlibs/math/atoms.h"
+#include "../../tlibs/helper/array.h"
+
 
 void list_sgs()
 {
@@ -55,12 +59,61 @@ void test_hkls()
 	}
 }
 
+
+// ----------------------------------------------------------------------------
+
+
+// ugly, but can't be helped for the moment:
+// directly link to the internal clipper coefficients table
+// that lives in clipper/clipper/core/atomsf.cpp
+namespace clipper { namespace data
+{
+	extern const struct SFData
+	{
+		const char atomname[8];
+		const double a[5], c, b[5], d;	// d is always 0
+	} sfdata[];
+
+	const int numsfdata = 212;
+}}
+
+
+void ffact()
+{
+	/*for(int i=0; i<clipper::data::numsfdata; ++i)
+	{
+		std::cout << clipper::data::sfdata[i].atomname << ": c = "
+			<< clipper::data::sfdata[i].c << std::endl;
+	}*/
+
+	const int iAtom = 1;	// He
+
+	std::string strAtom = clipper::data::sfdata[iAtom].atomname;
+	tl::wrapper_array<double> a = tl::wrapper_array<double>((double*)clipper::data::sfdata[iAtom].a, 5);
+	tl::wrapper_array<double> b = tl::wrapper_array<double>((double*)clipper::data::sfdata[iAtom].b, 5);
+	const double c = clipper::data::sfdata[iAtom].c;
+
+	std::cout << "# " << strAtom << "\n";
+	for(double G=0.; G<25.; G+=0.1)
+	{
+		const double ff = tl::formfact<double, tl::wrapper_array>(G, a, b, c);
+		std::cout << G << "\t" << ff << "\n";
+	}
+	std::cout.flush();
+}
+
+
+// ----------------------------------------------------------------------------
+
+
 int main()
 {
 	try
 	{
 		//list_sgs();
-		test_hkls();
+		//test_hkls();
+
+		ffact();
 	}
 	catch(const clipper::Message_fatal& ex)
 	{
