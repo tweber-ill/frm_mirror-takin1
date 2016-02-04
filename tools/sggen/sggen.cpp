f79328cac9de38b8fa48cd0d564bf9bea5305fdb
Tobias Weber <tobias.weber@frm2.tum.de>
2016-02-04 11:42:25 +0100

    created another small sg helper tool

--- tools/sggen/sggen.cpp
+++ tools/sggen/sggen.cpp
@@ -1,4 +1,4 @@
-// gcc -o sggen sggen.cpp -std=c++11 -lstdc++ -lm -I../.. -I/usr/include/QtGui/ ../../helper/spacegroup_clp.cpp ../../helper/crystalsys.cpp -DNO_QT -lclipper-core ../../tlibs/file/x3d.cpp
+// gcc -o sggen sggen.cpp -std=c++11 -lstdc++ -lm -I../.. -I/usr/include/QtGui/ ../../helper/spacegroup_clp.cpp ../../helper/crystalsys.cpp -DNO_QT -lclipper-core ../../tlibs/file/x3d.cpp ../../tlibs/log/log.cpp
 
 /**
  * generates atom positions based on space group
