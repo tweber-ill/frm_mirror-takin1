7215a0d4214a8232b2c67d8f0ede92c859b59891
Tobias Weber <tobias.weber@frm2.tum.de>
2016-02-24 09:33:23 +0100

    updated make files

--- Findtlibs.cmake
+++ Findtlibs.cmake
@@ -2,15 +2,21 @@ find_path(tlibs_INCLUDE_DIRS
 	NAMES version.h
 	PATH_SUFFIXES tlibs
 	HINTS /usr/include/tlibs /usr/local/include/tlibs /opt/local/include/tlibs
-	DOC "tlibs include directories"
+	DOC "tlibs include directory"
 )
-
 
 find_library(tlibs_LIBRARIES
 	NAMES libtlibs.so
 	HINTS /usr/lib64/ /usr/lib/ /usr/local/lib64 /usr/local/lib /opt/local/lib
 	DOC "tlibs library"
 )
+
+find_path(tlibs_SHARED_DIRS
+	NAMES AUTHORS COPYING.GPLv2 COPYING.GPLv3 LICENSES FindMinuit2.cmake
+	HINTS /usr/share/tlibs /usr/local/share/tlibs /opt/local/share/tlibs
+	DOC "tlibs shared directory"
+)
+
 
 if(tlibs_LIBRARIES)
 	set(tlibs_FOUND 1)
