fcebf5d2f8bb9d05a303b88f75bca34caf6a9abe
Tobias Weber <tobias.weber@frm2.tum.de>
2016-03-02 11:37:24 +0100

    added initial checks

--- tools/taz/taz_main.cpp
+++ tools/taz/taz_main.cpp
@@ -16,43 +16,21 @@
 #include <clocale>
 #include <memory>
 #include <QMetaType>
+#include <QMessageBox>
 
 
 #ifdef Q_WS_X11
 	extern "C" int XInitThreads();
 #endif
 
+
+#define TAKIN_CHECK " Please check if Takin is correctly installed and the current working directory is set to the Takin main directory."
+
 int main(int argc, char** argv)
 {
 	try
 	{
 		tl::log_info("Starting up Takin.");
-
-		// tlibs version check
-		tl::log_info("Using tLibs version ", tl::get_tlibs_version(), ".");
-		if(!tl::check_tlibs_version(TLIBS_VERSION))
-		{
-			tl::log_crit("Version mismatch in tLibs. Please recompile.");
-			tl::log_crit("tLibs versions: library: ", tl::get_tlibs_version(),
-				", headers: ", TLIBS_VERSION, ".");
-			return -1;
-		}
-
-		// check tables
-		g_bHasScatlens = (find_resource("res/scatlens.xml") != "");
-		g_bHasFormfacts = (find_resource("res/ffacts.xml") != "");
-		if(!g_bHasScatlens)
-			tl::log_warn("Scattering length table not found.");
-		if(!g_bHasFormfacts)
-			tl::log_warn("Form factor coefficient table not found.");
-
-		if(!init_space_groups())
-		{
-			tl::log_err("Space group table not found!");
-			return -1;
-		}
-		tl::init_spec_chars();
-
 
 		#if defined Q_WS_X11 && !defined NO_3D
 			XInitThreads();
@@ -65,12 +43,68 @@ int main(int argc, char** argv)
 		qRegisterMetaType<std::string>("std::string");
 		qRegisterMetaType<CacheVal>("CacheVal");
 
-
 		std::unique_ptr<QApplication> app(new QApplication(argc, argv));
-
 		std::setlocale(LC_ALL, "C");
 		QLocale::setDefault(QLocale::English);
 
+
+		// ------------------------------------------------------------
+		// tlibs version check
+		tl::log_info("Using tLibs version ", tl::get_tlibs_version(), ".");
+		if(!tl::check_tlibs_version(TLIBS_VERSION))
+		{
+			tl::log_crit("Version mismatch in tLibs. Please recompile.");
+			tl::log_crit("tLibs versions: library: ", tl::get_tlibs_version(),
+				", headers: ", TLIBS_VERSION, ".");
+
+			QMessageBox::critical(0, "Takin - Error", "Broken build: Mismatch in tlibs version.");
+			return -1;
+		}
+
+		// check tables
+		g_bHasScatlens = (find_resource("res/scatlens.xml") != "");
+		g_bHasFormfacts = (find_resource("res/ffacts.xml") != "");
+		if(!g_bHasScatlens)
+		{
+			const char* pcErr = "Scattering length table could not be found." TAKIN_CHECK;
+			tl::log_err(pcErr);
+
+			QMessageBox::critical(0, "Takin - Error", pcErr);
+			return -1;
+		}
+		if(!g_bHasFormfacts)
+		{
+			const char* pcErr = "Form factor coefficient table could not be found." TAKIN_CHECK;
+			tl::log_err(pcErr);
+
+			QMessageBox::critical(0, "Takin - Error", pcErr);
+			return -1;
+		}
+
+		if(!init_space_groups())
+		{
+			const char* pcErr = "Space group table could not be found!" TAKIN_CHECK;
+			tl::log_err(pcErr);
+
+			QMessageBox::critical(0, "Takin - Error", pcErr);
+			return -1;
+		}
+
+		tl::init_spec_chars();
+
+
+		// check if icons are available
+		if(find_resource("res/document-new.svg") == "")
+		{
+			const char* pcErr = "Takin resources could not be found!" TAKIN_CHECK;
+			tl::log_err(pcErr);
+
+			QMessageBox::critical(0, "Takin - Error", pcErr);
+			return -1;
+		}
+
+
+		// ------------------------------------------------------------
 		std::unique_ptr<TazDlg> dlg(new TazDlg(0));
 		if(argc > 1)
 			dlg->Load(argv[1]);
@@ -78,6 +112,7 @@ int main(int argc, char** argv)
 		int iRet = app->exec();
 
 
+		// ------------------------------------------------------------
 		tl::deinit_spec_chars();
 		tl::log_info("Shutting down Takin.");
 		return iRet;
@@ -86,6 +121,5 @@ int main(int argc, char** argv)
 	{
 		tl::log_crit(ex.what());
 	}
-
 	return -1;
 }
