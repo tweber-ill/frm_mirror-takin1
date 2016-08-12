807b02f3d261a0e7f4d7d459592b1d06df3ea294
Tobias Weber <tobias.weber@frm2.tum.de>
2016-08-12 13:53:47 +0200

    improved handling of convofit program args

--- tools/convofit/convofit.cpp
+++ tools/convofit/convofit.cpp
@@ -21,6 +21,7 @@
 #include <boost/asio/io_service.hpp>
 #include <boost/asio/signal_set.hpp>
 #include <boost/scope_exit.hpp>
+#include <boost/program_options.hpp>
 
 #include "convofit.h"
 #include "scan.h"
@@ -28,11 +29,15 @@
 #include "../monteconvo/sqwfactory.h"
 #include "../res/defs.h"
 
+
 //using t_real = tl::t_real_min;
 using t_real = t_real_reso;
 
 namespace asio = boost::asio;
 namespace sys = boost::system;
+namespace opts = boost::program_options;
+
+bool g_bSkipFit;
 
 
 bool run_job(const std::string& strJob)
@@ -142,7 +147,7 @@ bool run_job(const std::string& strJob)
 	int iStrat = prop.Query<int>("fitter/strategy", 0);
 	t_real dSigma = prop.Query<t_real>("fitter/sigma", 1.);
 
-	bool bDoFit = prop.Query<bool>("fitter/do_fit", 1);
+	bool bDoFit = prop.Query<bool>("fitter/do_fit", 1) || g_bSkipFit;
 	unsigned int iMaxFuncCalls = prop.Query<unsigned>("fitter/max_funccalls", 0);
 	t_real dTolerance = prop.Query<t_real>("fitter/tolerance", 0.5);
 
@@ -614,92 +619,141 @@ bool run_job(const std::string& strJob)
 
 int main(int argc, char** argv)
 {
-#ifdef NO_TERM_CMDS
-	tl::Log::SetUseTermCmds(0);
-#endif
-
-	// plain C locale
-	/*std::*/setlocale(LC_ALL, "C");
-	std::locale::global(std::locale::classic());
-
-	// install exit signal handlers
-	asio::io_service ioSrv;
-	asio::signal_set sigInt(ioSrv, SIGABRT, SIGTERM, SIGINT);
-	sigInt.async_wait([&ioSrv](const sys::error_code& err, int iSig)
+	try
 	{
-		tl::log_warn("Hard exit requested via signal ", iSig, ". This may cause a fault.");
-		if(err) tl::log_err("Error: ", err.message(), ", error category: ", err.category().name(), ".");
-		ioSrv.stop();
-#ifdef SIGKILL
-		std::system("killall -s KILL gnuplot");
-		std::raise(SIGKILL);
-#endif
-		exit(-1);
-	});
-	std::thread thSig([&ioSrv]() { ioSrv.run(); });
-	BOOST_SCOPE_EXIT(&ioSrv, &thSig)
-	{
-		//tl::log_debug("Exiting...");
-		ioSrv.stop();
-		thSig.join();
-	}
-	BOOST_SCOPE_EXIT_END
+	#ifdef NO_TERM_CMDS
+		tl::Log::SetUseTermCmds(0);
+	#endif
+
+		// plain C locale
+		/*std::*/setlocale(LC_ALL, "C");
+		std::locale::global(std::locale::classic());
 
 
-	tl::log_info("This is the Takin command-line convolution fitter, version " TAKIN_VER ".");
-	tl::log_info("Written by Tobias Weber <tobias.weber@tum.de>, 2014-2016.");
-	tl::log_debug("Resolution calculation uses ", sizeof(t_real_reso)*8, " bit ", tl::get_typename<t_real_reso>(), "s.");
-	tl::log_debug("Fitting uses ", sizeof(tl::t_real_min)*8, " bit ", tl::get_typename<tl::t_real_min>(), "s.");
-
-	if(argc > 2)
-	{
-		for(tl::Log* log : { &tl::log_info, &tl::log_warn, &tl::log_err, &tl::log_crit, &tl::log_debug })
-			log->SetShowThread(1);
-	}
-
-	if(argc <= 1)
-	{
-		tl::log_info("Usage:");
-		tl::log_info("\t", argv[0], " <job file 1> <job file 2> ...");
-		return -1;
-	}
-
-
-	unsigned int iNumThreads = std::thread::hardware_concurrency();
-	tl::ThreadPool<bool()> tp(iNumThreads);
-
-	for(int iArg=1; iArg<argc; ++iArg)
-	{
-		std::string strJob = argv[iArg];
-		tp.AddTask([iArg, strJob]() -> bool
+		// --------------------------------------------------------------------
+		// install exit signal handlers
+		asio::io_service ioSrv;
+		asio::signal_set sigInt(ioSrv, SIGABRT, SIGTERM, SIGINT);
+		sigInt.async_wait([&ioSrv](const sys::error_code& err, int iSig)
 		{
-			tl::log_info("Executing job file ", iArg, ": \"", strJob, "\".");
-
-			return run_job(strJob);
-			//if(argc > 2) tl::log_info("================================================================================");
+			tl::log_warn("Hard exit requested via signal ", iSig, ". This may cause a fault.");
+			if(err) tl::log_err("Error: ", err.message(), ", error category: ", err.category().name(), ".");
+			ioSrv.stop();
+	#ifdef SIGKILL
+			std::system("killall -s KILL gnuplot");
+			std::raise(SIGKILL);
+	#endif
+			exit(-1);
 		});
+		std::thread thSig([&ioSrv]() { ioSrv.run(); });
+		BOOST_SCOPE_EXIT(&ioSrv, &thSig)
+		{
+			//tl::log_debug("Exiting...");
+			ioSrv.stop();
+			thSig.join();
+		}
+		BOOST_SCOPE_EXIT_END
+		// --------------------------------------------------------------------
+
+
+		tl::log_info("This is the Takin command-line convolution fitter, version " TAKIN_VER ".");
+		tl::log_info("Written by Tobias Weber <tobias.weber@tum.de>, 2014-2016.");
+		tl::log_debug("Resolution calculation uses ", sizeof(t_real_reso)*8, " bit ", tl::get_typename<t_real_reso>(), "s.");
+		tl::log_debug("Fitting uses ", sizeof(tl::t_real_min)*8, " bit ", tl::get_typename<tl::t_real_min>(), "s.");
+
+
+		// --------------------------------------------------------------------
+		// get job files and program options
+		std::vector<std::string> vecJobs;
+
+		// normal args
+		opts::options_description args("convofit options");
+		args.add(boost::shared_ptr<opts::option_description>(
+			new opts::option_description("job-file", opts::value<decltype(vecJobs)>(), "convolution fitting job file")));
+		args.add(boost::shared_ptr<opts::option_description>(
+			new opts::option_description("skip-fit", opts::bool_switch(), "skip the fitting step")));
+
+		// positional args
+		opts::positional_options_description args_pos;
+		args_pos.add("job-file", -1);
+
+		opts::basic_command_line_parser<char> clparser(argc, argv);
+		clparser.options(args);
+		clparser.positional(args_pos);
+		opts::basic_parsed_options<char> parsedopts = clparser.run();
+
+		opts::variables_map opts_map;
+		opts::store(parsedopts, opts_map);
+		opts::notify(opts_map);
+
+		g_bSkipFit = (opts_map.count("skip-fit") != 0);
+		if(opts_map.count("job-file"))
+			vecJobs = opts_map["job-file"].as<decltype(vecJobs)>();
+
+
+		if(vecJobs.size() >= 2)
+		{
+			for(tl::Log* log : { &tl::log_info, &tl::log_warn, &tl::log_err, &tl::log_crit, &tl::log_debug })
+				log->SetShowThread(1);
+		}
+
+		if(argc <= 1)
+		{
+			std::ostringstream ostrHelp;
+			ostrHelp << "Usage: " << argv[0] << " [options] <job-file 1> <job-file 2> ...\n";
+			ostrHelp << args;
+			tl::log_info(ostrHelp.str());
+			return -1;
+		}
+
+		if(vecJobs.size() == 0)
+		{
+			tl::log_err("No job files given.");
+			return -1;
+		}
+		// --------------------------------------------------------------------
+
+
+		unsigned int iNumThreads = std::thread::hardware_concurrency();
+		tl::ThreadPool<bool()> tp(iNumThreads);
+
+		for(std::size_t iJob=0; iJob<vecJobs.size(); ++iJob)
+		{
+			const std::string& strJob = vecJobs[iJob];
+			tp.AddTask([iJob, strJob]() -> bool
+			{
+				tl::log_info("Executing job file ", iJob+1, ": \"", strJob, "\".");
+
+				return run_job(strJob);
+				//if(argc > 2) tl::log_info("================================================================================");
+			});
+		}
+
+		tl::Stopwatch<t_real> watch;
+		watch.start();
+		tp.StartTasks();
+
+		auto& lstFut = tp.GetFutures();
+		std::size_t iTask = 0;
+		for(auto& fut : lstFut)
+		{
+			bool bOk = fut.get();
+			if(!bOk)
+				tl::log_err("Job ", iTask+1, " (", vecJobs[iTask], ") failed or fit invalid!");
+			++iTask;
+		}
+
+		watch.stop();
+		tl::log_info("================================================================================");
+		tl::log_info("Start time:     ", watch.GetStartTimeStr());
+		tl::log_info("Stop time:      ", watch.GetStopTimeStr());
+		tl::log_info("Execution time: ", tl::get_duration_str_secs<t_real>(watch.GetDur()));
+		tl::log_info("================================================================================");
 	}
-
-	tl::Stopwatch<t_real> watch;
-	watch.start();
-	tp.StartTasks();
-
-	auto& lstFut = tp.GetFutures();
-	unsigned int iTask = 1;
-	for(auto& fut : lstFut)
+	catch(const std::exception& ex)
 	{
-		bool bOk = fut.get();
-		if(!bOk)
-			tl::log_err("Job ", iTask, " (", argv[iTask], ") failed or fit invalid!");
-		++iTask;
+		tl::log_crit(ex.what());
 	}
-
-	watch.stop();
-	tl::log_info("================================================================================");
-	tl::log_info("Start time:     ", watch.GetStartTimeStr());
-	tl::log_info("Stop time:      ", watch.GetStopTimeStr());
-	tl::log_info("Execution time: ", tl::get_duration_str_secs<t_real>(watch.GetDur()));
-	tl::log_info("================================================================================");
 
 	return 0;
 }
