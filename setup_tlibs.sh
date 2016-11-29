bd7854a5a23838c2dbf9ea73561003b2f2faa969
Tobias Weber <tobias.weber@frm2.tum.de>
2016-11-29 08:24:00 +0100

    version 1.0.5

--- setup_tlibs.sh
+++ setup_tlibs.sh
@@ -6,8 +6,8 @@ if [ $# -ge 1  -a  "$1" == "latest" ]; then
 	TLIBS=tlibs-master
 	TDATA=takin-data-master
 else
-	#TLIBS=tlibs-0.7.5
-	TLIBS=tlibs-master	# override for non-tagged version
+	TLIBS=tlibs-0.7.6
+	#TLIBS=tlibs-master	# override for non-tagged version
 
 	TDATA=takin-data-master
 fi
